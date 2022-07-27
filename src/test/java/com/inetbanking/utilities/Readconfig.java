ba4dd5d38bd6210b5eb3480235f5a167c3147c45
lohit6897 <lohitkumar.325@gmail.com>
2022-07-27 08:03:34 +0530

    cofiguration utility added

---
+++ src/test/java/com/inetbanking/utilities/Readconfig.java
@@ -0,0 +1,62 @@
+package com.inetbanking.utilities;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class Readconfig {
+	Properties pro;
+	public Readconfig()
+	{
+		File src=new File("./Configuration/Config.properties");
+		try
+		{
+			FileInputStream fis=new FileInputStream(src);
+			pro=new Properties();
+			pro.load(fis);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Exception is:"+e.getMessage());
+		}
+	}
+
+	public String getApplicationURL()
+	{
+		String url=pro.getProperty("baseURL");
+		return url;
+	}
+	public String getusername()
+	{
+		String username=pro.getProperty("username");
+		return username ;
+	}
+	public String getpassword()
+	{
+		String pwd=pro.getProperty("password");
+		return pwd;
+	}
+	public String getchromepathL()
+	{
+		String chromepath=pro.getProperty("chromepath");
+		return chromepath;
+	}
+	public String getIEpath()
+	{
+		String iepath=pro.getProperty("iepath");
+		return iepath;
+	}
+	public String getfirefoxpath()
+	{
+		String firepath=pro.getProperty("firefoxpath");
+		return firepath;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
